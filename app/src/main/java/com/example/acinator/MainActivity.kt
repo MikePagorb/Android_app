47630e65fd5dc37af4e4d3f0e6727c771ce8ad06
Mikhailo Kholmohorov <mykhailo1257@gmail.com>
2020-01-24 22:46:20 +0200

    Add function "Error"

--- app/src/main/java/com/example/acinator/MainActivity.kt
+++ app/src/main/java/com/example/acinator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun textError (){//видає повідомлення про невведений рядок з пісні
+        val error = Toast.makeText(this,"Enter line", Toast.LENGTH_SHORT)
+        error.show()
+    }
     fun DoneSecondPage (view: View){//функція для кнопки Done щоб перейти на некст сторінку
-        if(editText.text.toString() !=""||editText.text.toString() != null){// перевірка на введення рядка пісні
+        if(editText.text.toString() !=""){// перевірка на введення рядка пісні
         val doneButton = Intent(this,SecondActivity::class.java)
         val nameOfSong = editText.text.toString()
         doneButton.putExtra(SecondActivity.TOTAL_STRING, nameOfSong)
         startActivity(doneButton)
-        }
+        }else textError()
     }
+
 }
